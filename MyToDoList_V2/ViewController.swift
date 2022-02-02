4aa90f40a06341c68ae392e1f6b4a840bbaa0815
Anna <anna.vasi4ko@icloud.com>
2022-02-02 17:07:21 +0300

    Initial Commit

---
+++ MyToDoList_V2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyToDoList_V2
+//
+//  Created by Vasichko Anna on 02.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
